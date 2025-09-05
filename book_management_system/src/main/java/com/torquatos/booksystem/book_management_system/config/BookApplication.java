ca79cfcf4eb23cb83aebaab8bed616b62f0a82cd
torquatos <226768122+torquatos@users.noreply.github.com>
2025-09-05 09:05:51 -0300

    initiating project

--- book_management_system/src/main/java/com/torquatos/booksystem/book_management_system/config/BookApplication.java
+++ book_management_system/src/main/java/com/torquatos/booksystem/book_management_system/config/BookApplication.java
@@ -5,3 +5,8 @@ import org.springframework.boot.*;
 import org.springframework.context.*;
 
 import java.sql.*;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.torquatos.booksystem.book_management_system.config")
+
+public class BookApplication
