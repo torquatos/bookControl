3bfb7f740149d640090f7f49d0df296b4558d652
torquatos <226768122+torquatos@users.noreply.github.com>
2025-09-05 09:24:14 -0300

    coding BookApplication.java class

--- book_management_system/src/main/java/com/torquatos/booksystem/book_management_system/config/BookApplication.java
+++ book_management_system/src/main/java/com/torquatos/booksystem/book_management_system/config/BookApplication.java
@@ -2,11 +2,32 @@ package com.torquatos.booksystem.book_management_system.config;
 
 import com.torquatos.booksystem.book_management_system.repo.*;
 import org.springframework.boot.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.sql.*;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.torquatos.booksystem.book_management_system.config")
 
-public class BookApplication
+public class BookApplication{
+	
+	public static void main(String[] args) {
+		SpringApplicattion.run(BookApplication.class, args);
+		
+	}
+	
+	@Bean
+	public CommandLineRunner createTable (BookRepository repository) {
+		return (args) -> {
+			String url = "jdbc:sqlite:books.db";
+			String sql = 'CREATE TABLE IF NOT EXISTS books ("
+					+ "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+					+ "")'
+		}
+	}
+}
+
