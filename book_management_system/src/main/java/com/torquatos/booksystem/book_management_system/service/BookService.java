d76a367466c9bb38bddbf515d23cc7e4cb30508c
torquatos <226768122+torquatos@users.noreply.github.com>
2025-09-08 15:41:19 -0300

    fixing bug

--- book_management_system/src/main/java/com/torquatos/booksystem/book_management_system/service/BookService.java
+++ book_management_system/src/main/java/com/torquatos/booksystem/book_management_system/service/BookService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.*;
 @Service
 public class BookService {
 	
-	private final BookRepsitory bookRepository;
+	private final BookRepository bookRepository;
 	
 	@Autowired
 	public BookService(BookRepository bookRepository) {
