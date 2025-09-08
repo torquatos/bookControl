c55d47ab6413ef02016a293057d92a5363bc8fe8
torquatos <226768122+torquatos@users.noreply.github.com>
2025-09-08 11:49:06 -0300

    starting book.java in entity

---
+++ book_management_system/src/main/java/com/torquatos/booksystem/book_management_system/entity/Book.java
@@ -0,0 +1,32 @@
+package com.torquatos.booksystem.book_management_system.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "books")
+
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	private String author;
+	private String genre;
+	private String publisher;
+	private int year;
+	private String summary;
+	private boolean wasRead;
+	private double rate;
+	private String personalNotes;
+	private boolean readAgain;
+	
+	public Book() {
+		
+	}
+	
+	public Book()
+
+}
