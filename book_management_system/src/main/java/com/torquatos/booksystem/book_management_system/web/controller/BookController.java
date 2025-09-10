a522a2858ee6229e9b2fe12692510e740af1d26d
torquatos <226768122+torquatos@users.noreply.github.com>
2025-09-10 06:25:53 -0300

    coding controller get mapping id

--- book_management_system/src/main/java/com/torquatos/booksystem/book_management_system/web/controller/BookController.java
+++ book_management_system/src/main/java/com/torquatos/booksystem/book_management_system/web/controller/BookController.java
@@ -4,10 +4,26 @@ import com.torquatos.booksystem.book_management_system.entity.*;
 import com.torquatos.booksystem.book_management_system.service.*;
 
 import org.springframework.*;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @RestController
 @RequestMapping("/api/books")
-public class BookController {}
+public class BookController {
+	
+	private final BookService bookService;
+	
+	@Autowired
+	public BookController(BookService bookService) {
+		this.bookService = bookService;
+		}
+	
+	@GetMapping
+	public List<Book> getAllBooks(){
+		return bookService.getAllBooks();
+	}
+	
+	
+}
