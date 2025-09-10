762e232bc7bb7eac87666f668b0427c56a4cce51
torquatos <226768122+torquatos@users.noreply.github.com>
2025-09-10 06:47:23 -0300

    fixing @PutMaping

--- book_management_system/src/main/java/com/torquatos/booksystem/book_management_system/web/controller/BookController.java
+++ book_management_system/src/main/java/com/torquatos/booksystem/book_management_system/web/controller/BookController.java
@@ -40,7 +40,7 @@ public class BookController {
 		return new ResponseEntity<>(createdBook, HttpStatus.CREATED);
 	}
 	
-	@PutMapping
+	@PutMapping("/{id}")
 	public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book){
 		return bookService.getBookById(id)
 				.map(existingBook -> {
