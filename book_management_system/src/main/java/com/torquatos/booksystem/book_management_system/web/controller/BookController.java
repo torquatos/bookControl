372902d6531a09a075008156e8ff66929f2fc0ff
torquatos <226768122+torquatos@users.noreply.github.com>
2025-09-10 06:46:05 -0300

    editing @PutMaping

--- book_management_system/src/main/java/com/torquatos/booksystem/book_management_system/web/controller/BookController.java
+++ book_management_system/src/main/java/com/torquatos/booksystem/book_management_system/web/controller/BookController.java
@@ -5,6 +5,8 @@ import com.torquatos.booksystem.book_management_system.service.*;
 
 import org.springframework.*;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -25,5 +27,26 @@ public class BookController {
 		return bookService.getAllBooks();
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Book> getBookById(@PathVariable Long id){
+		return bookService.getBookById(id)
+				.map(task -> new ResponseEntity<>(task, HttpStatus.OK))
+				.orElseGet(()-> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
 	
+	@PostMapping
+	public ResponseEntity<Book> createBook(@RequestBody Book book){
+		Book createdBook = bookService.saveBook(book);
+		return new ResponseEntity<>(createdBook, HttpStatus.CREATED);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book){
+		return bookService.getBookById(id)
+				.map(existingBook -> {
+					bookService.updateBook(id, book);
+					return new ResponseEntity<>(book, HttpStatus.OK);				
+				})
+				.orElseGet(() -> new ResponseEntity<>(book, HttpStatus.NOT_FOUND));
+	}
 }
